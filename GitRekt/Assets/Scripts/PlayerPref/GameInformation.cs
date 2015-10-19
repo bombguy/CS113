c3da63f492667452ddfe8dcb94d78fb99055c68b
Jonathan Hammel <Jonathan@Jonathans-MacBook-Pro.local>
2015-10-19 10:22:25 -0700

    basic load/save scripts. need more information on what we are saving

---
+++ GitRekt/Assets/Scripts/PlayerPref/GameInformation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameInformation : MonoBehaviour {
+
+
+	void Awake(){
+		DontDestroyOnLoad (transform.gameObject);
+	}
+
+	public static int level(get;set;)
+//	// Use this for initialization
+//	void Start () {
+//	
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//	
+//	}
+//}
+}
---
+++ GitRekt/Assets/Scripts/PlayerPref/SaveInformation.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SaveInformation{
+
+	public static void SaveAllinformation(){
+		PlayerPrefs.SetInt ("PLAYERLEVEL", 1);
+		//Include all the other information that we want to save
+	}
+}
