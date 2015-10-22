6642756c704f702ebae16ae56e9237988558f0d4
merodri2 <merodri2@uci.edu>
2015-10-22 14:35:57 -0700

    Update GameInformation.cs

--- GitRekt/Assets/Scripts/GameInformation.cs
+++ GitRekt/Assets/Scripts/GameInformation.cs
@@ -9,11 +9,11 @@ public class GameInformation:MonoBehaviour {
     public static baseSkill Spell3 { get; set; }
     public static baseSkill Spell4 { get; set; }
 
-    public static int Health { get; set; }
-    public static int Attack { get; set; }
-    public static int Defense { get; set; }
-    public static int FlowMastery { get; set; }
-    public static int FunctionMastery { get; set; }
-    public static int DataStructureMastery { get; set; }
-    public static int NetworkMastery { get; set; }
+    public static int playerHealth { get; set; }
+    public static int playerAttack { get; set; }
+    public static int playerDefense { get; set; }
+    public static int playerFlowMastery { get; set; }
+    public static int playerFunctionMastery { get; set; }
+    public static int playerDataStructureMastery { get; set; }
+    public static int playerNetworkMastery { get; set; }
 }
