3759d603646ce2972daebf881d66f9f159f55c68
merodri2 <merodri2@uci.edu>
2015-10-29 14:58:34 -0700

    Art Added  -Sprites, animators and animations added Prefabs Added  - Prefabs for tile floors, and player added Scripts added  - dev on the battle state and GUI script for running the state     - Battle UI now generates buttons for player spells 	*Logic will go here to move the turn along     - Once pressed the state goes to enemy mode     - Start Stated added         Responsible for generating enemies Scene added  - Battle scene added. We could procedually generate floor tiles    to build different enviornments with one base scene.

---
+++ GitRekt/Assets/Scripts/BattleGUI.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+    GUI for battles
+    Player Decesions will be ran through here
+    UI workflow:
+        if(touch player)
+            if(Spell choice)
+                if(Target choice)
+                    conduct combat
+*/
+public class BattleGUI : MonoBehaviour {
+
+    private string playerName;
+    private int playerHealth;
+    
+     
+
+	// Use this for initialization
+	void Start () {
+        playerName = GameInformation.Player.PlayerName;
+        playerHealth = GameInformation.Player.Health;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+
+    void OnGUI()
+    {
+        if (CombatStateMachine.currentState == CombatStateMachine.CombatStates.PLAYER)
+            DisplayPlayerChoices();
+
+    }
+
+    private void DisplayPlayerChoices(){
+        if (GUI.Button(new Rect(Screen.width - 500, Screen.height - 50, 100, 30), "Spell1")){
+            CombatStateMachine.currentState = CombatStateMachine.CombatStates.ENEMY;
+        }
+        if (GUI.Button(new Rect(Screen.width - 400, Screen.height - 50, 100, 30), "Spell2")){
+            CombatStateMachine.currentState = CombatStateMachine.CombatStates.ENEMY;
+        }
+        if (GUI.Button(new Rect(Screen.width - 300, Screen.height - 50, 100, 30), "Spell3")){
+            CombatStateMachine.currentState = CombatStateMachine.CombatStates.ENEMY;
+        }
+        if (GUI.Button(new Rect(Screen.width - 200, Screen.height - 50, 100, 30), "Spell4")){
+            CombatStateMachine.currentState = CombatStateMachine.CombatStates.ENEMY;
+        }
+        if (GUI.Button(new Rect(Screen.width - 100, Screen.height - 50, 100, 30), "Basic Attack")){
+            CombatStateMachine.currentState = CombatStateMachine.CombatStates.ENEMY;
+        }
+    }
+}
