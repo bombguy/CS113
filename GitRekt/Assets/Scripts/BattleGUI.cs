034088fd3f9c69d6eafb06d4860b5f689857c385
merodri2 <merodri2@uci.edu>
2015-11-02 22:52:22 -0800

    Added health for Legacy GUI (Will work on event driven soon) Added BattleManager, BoardManager for level control/Generation. Added test player, just a shell script to begin testing combat cases Added test skill, Shell script to begin testing character/skill relationship and battle mechanics

--- GitRekt/Assets/Scripts/BattleGUI.cs
+++ GitRekt/Assets/Scripts/BattleGUI.cs
@@ -14,43 +14,47 @@ public class BattleGUI : MonoBehaviour {
 
     private string playerName;
     private int playerHealth;
-    
+    private string playerHealthLabel;
      
 
 	// Use this for initialization
 	void Start () {
-        playerName = GameInformation.Player.PlayerName;
-        playerHealth = GameInformation.Player.Health;
+        playerName = "Test";
+        playerHealth = 100;
+        playerHealthLabel = playerHealth.ToString();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+       
 	}
-    
 
+    public int PlayerHealth { get; set; }
     void OnGUI()
     {
-        if (CombatStateMachine.currentState == CombatStateMachine.CombatStates.PLAYER)
+        GUI.Label(new Rect(10, 10, 100, 20), playerHealthLabel);
+        if (CombatStateMachine.CurrentState == CombatStateMachine.CombatStates.PLAYER)
             DisplayPlayerChoices();
 
     }
 
     private void DisplayPlayerChoices(){
         if (GUI.Button(new Rect(Screen.width - 500, Screen.height - 50, 100, 30), "Spell1")){
-            CombatStateMachine.currentState = CombatStateMachine.CombatStates.ENEMY;
+            CombatStateMachine.CurrentState = CombatStateMachine.CombatStates.ENEMY;
         }
         if (GUI.Button(new Rect(Screen.width - 400, Screen.height - 50, 100, 30), "Spell2")){
-            CombatStateMachine.currentState = CombatStateMachine.CombatStates.ENEMY;
+            CombatStateMachine.CurrentState = CombatStateMachine.CombatStates.ENEMY;
         }
         if (GUI.Button(new Rect(Screen.width - 300, Screen.height - 50, 100, 30), "Spell3")){
-            CombatStateMachine.currentState = CombatStateMachine.CombatStates.ENEMY;
+            CombatStateMachine.CurrentState = CombatStateMachine.CombatStates.ENEMY;
         }
         if (GUI.Button(new Rect(Screen.width - 200, Screen.height - 50, 100, 30), "Spell4")){
-            CombatStateMachine.currentState = CombatStateMachine.CombatStates.ENEMY;
+            CombatStateMachine.CurrentState = CombatStateMachine.CombatStates.ENEMY;
         }
         if (GUI.Button(new Rect(Screen.width - 100, Screen.height - 50, 100, 30), "Basic Attack")){
-            CombatStateMachine.currentState = CombatStateMachine.CombatStates.ENEMY;
+            playerHealth = Utilities.combat(playerHealth, 10);
+            playerHealthLabel = playerHealth.ToString();
+            CombatStateMachine.CurrentState = CombatStateMachine.CombatStates.ENEMY;
         }
     }
 }
--- GitRekt/Assets/Scripts/Utilities.cs
+++ GitRekt/Assets/Scripts/Utilities.cs
@@ -6,7 +6,17 @@ using System.Collections;
 //By various classes.
 public static class Utilities{
     public static void calculateStat() { }
-    public static void calculateDamage() { }
+
+
+    public static int combat(int targetHealth, int friendlyDamage) { return targetHealth - friendlyDamage; }
+    public static int combat(baseSkill ability, basePlayer target) {return target.Health - CalculateSpellDamage(ability, 0);}
+    public static int heal(int targetHealth, int friendlyHeal) { return targetHealth + friendlyHeal; }
+    public static int heal(baseSkill ability, basePlayer target) { return target.Health + CalculateSpellDamage(ability, 0); }
+
+
+    public static int CalculateSpellDamage(baseSkill usedAbility, int scalarModifier ) {return ((usedAbility.Power * scalarModifier) + usedAbility.Power);}
+
+
     public static int generateStat(int value, float modifier) {
         return (int)Random.Range(value - (value * modifier), value + (value * modifier));
     }
