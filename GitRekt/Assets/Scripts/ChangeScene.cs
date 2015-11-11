272ed94466de85809bef945b1bd391d13e61fd0e
Michelle Wong <michellewong.nma@gmail.com>
2015-11-10 18:04:50 -0800

    Made scenes change using string instead of int

--- GitRekt/Assets/Scripts/ChangeScene.cs
+++ GitRekt/Assets/Scripts/ChangeScene.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class ChangeScene : MonoBehaviour {
 
-	public void ChangeToScene(int sceneToChangeTo){
+	public void ChangeToScene(string sceneToChangeTo){
 		//give index to each scene and call them by their index(int)
 		//note: can check index from build menu and change index of scenes
 		Application.LoadLevel(sceneToChangeTo);
