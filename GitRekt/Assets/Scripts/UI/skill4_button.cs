db70f924cae783f8365a77343aed9042ccb512f9
merodri2 <merodri2@uci.edu>
2015-11-25 19:44:16 -0800

    ActionBar now visually shows spells. Adding Selection Logic.

---
+++ GitRekt/Assets/Scripts/UI/skill4_button.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class skill4_button : MonoBehaviour {
+    Button skillButton;
+    Image skillImage;
+    Text skillName;
+    baseSkill current_skill;
+	// Use this for initialization
+	void Start () {
+        skillImage = GetComponent<Image>();
+        skillButton = GetComponent<Button>();
+        skillName = GetComponentInChildren<Text>();
+        testButton();
+	}
+
+	// Update is called once per frame
+	void Update () {
+        //updateSkill();
+	}
+    void updateSkill() {
+        if (BattleManager.selectedUnit == null)
+        {
+        }
+        else {
+            if (current_skill == null)
+                addSkilltoButton(BattleManager.selectedUnit.skill4);
+            else if (current_skill != BattleManager.selectedUnit.skill4)
+                addSkilltoButton(BattleManager.selectedUnit.skill4);
+        }
+    }
+
+    void addSkilltoButton(baseSkill in_skill) {
+        current_skill = in_skill;
+        skillImage.sprite = Resources.Load<Sprite>("Skill/" + current_skill.skillName.ToLower());
+        skillName.text = in_skill.skillName;
+        skillButton.image = skillImage;
+    }
+    void testButton() {
+        baseSkill test = new BreakAndContinue();
+        addSkilltoButton(test);
+    }
+}
